c57563b52b71ab017668df873582dcd124732f94
beezex <46980424+beezex@users.noreply.github.com>
2019-06-15 22:15:06 -0400

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -118,7 +118,7 @@
 
 #define ALLOW_DEBUG_COMMANDS
 
-#define CRYPTONOTE_NAME                         "caliber"
+#define CRYPTONOTE_NAME                         "BitcoinCN"
 #define CRYPTONOTE_POOLDATA_FILENAME            "poolstate.bin"
 #define CRYPTONOTE_BLOCKCHAINDATA_FILENAME      "data.mdb"
 #define CRYPTONOTE_BLOCKCHAINDATA_LOCK_FILENAME "lock.mdb"
@@ -145,16 +145,17 @@ namespace config
   uint64_t const BASE_REWARD_CLAMP_THRESHOLD = ((uint64_t)100000000); // pow(10, 8)
   std::string const P2P_REMOTE_DEBUG_TRUSTED_PUB_KEY = "0000000000000000000000000000000000000000000000000000000000000000";
 
-  uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 0x316954;
-  uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 0xfd12954;
-  uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 0x1ad12954;
-  uint16_t const P2P_DEFAULT_PORT = 19870;
-  uint16_t const RPC_DEFAULT_PORT = 19871;
-  uint16_t const ZMQ_RPC_DEFAULT_PORT = 19872;
+  uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 0x34cbd0;
+  uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 0x34cbd0;
+  uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 0x34cbd0;
+  uint16_t const P2P_DEFAULT_PORT = 17237;
+  uint16_t const RPC_DEFAULT_PORT = 18237;
+
+  uint16_t const ZMQ_RPC_DEFAULT_PORT = 19237;
   boost::uuids::uuid const NETWORK_ID = { {
       0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x01 , 0x41, 0x62, 0x14, 0x36, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x14
     } }; // Bender's nightmare
-  std::string const GENESIS_TX = "023c01ff00018080c8dcbda1dfbf0202ea29f65c2925c0538cf10996551a3f061f2b97643ccc20b0b5593bce8a33f37221010c90ce4ed0236f969d3466b5dc3fc5da30ba630b474bf11206eb9e790bb32e4b00";
+  std::string const GENESIS_TX = "";
 
 
   uint32_t const GENESIS_NONCE = 10000;
@@ -170,7 +171,7 @@ namespace config
     boost::uuids::uuid const NETWORK_ID = { {
         0x12 ,0x30, 0xF1, 0x71 , 0x69, 0x04 , 0x46, 0x61, 0x17, 0x31, 0x04, 0x82, 0x11, 0xA1, 0xA1, 0x11
       } }; // Bender's daydream
-    std::string const GENESIS_TX = "023c01ff00018080c8dcbda1dfbf02026e3323d2df2c61ce60ea66d053616a4b8abfeb01f137d26359fb7471bd0684b82101b3f879b2af64e4daf4d1b491a0bec945798a3ea8894a5d4f21f4dae580cb80c600";
+    std::string const GENESIS_TX = "";
     uint32_t const GENESIS_NONCE = 10001;
   }
 
@@ -185,7 +186,7 @@ namespace config
     boost::uuids::uuid const NETWORK_ID = { {
         0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x60, 0x12, 0x30, 0x00, 0x82, 0x12, 0xA1, 0xA1, 0x12
       } }; // Bender's daydream
-    std::string const GENESIS_TX = "023c01ff00018080c8dcbda1dfbf02027807cd0e921a72204b8fb37f999d7aca0271eb36b403bf3b3ea1634499371f622101e9f5da74147d0cac2685183d90f367125791dfa66d820876c33ca11306e1ed8200";
+    std::string const GENESIS_TX = "";
     uint32_t const GENESIS_NONCE = 10002;
   }
 }
